e3179584c8526d27ff753158aa7aa4e68716d3f2
Ali Noroozbeigy <ali.noroozbeigy@gmail.com>
2021-03-13 22:02:21 +0330

    first session tasks

---
+++ first-session/src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // nesbat be ham aval
+        /*int num1=6 , num2=27;
+        int i =2;
+        boolean flag = false;
+
+        while (i!=num1 && i!=num2)
+        {
+            if (num1%i==0 && num2%i==0)
+            {
+                flag = true;
+                break;
+            }
+            i++;
+        }
+
+        if (flag)
+            System.out.println("nesbat be ham aval");
+        else System.out.println("false!!");*/
+
+        // jadval zarb
+        /*for (int i=1;i<=10;i++)
+        {
+            for (int j=1;j<=10;j++)
+            {
+                System.out.print((i*j)+"  ");
+            }
+            System.out.println();
+            System.out.println();
+        }*/
+
+        //main argument
+        /*for(String s : args)
+        {
+            System.out.println(s);
+        }*/
+
+    }
+}
